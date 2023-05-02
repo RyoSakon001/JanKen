57e126b73a22c7bc211503e858d1883af2050bf8
sakon-ziwave <ryo.development0320@gmail.com>
2023-05-03 07:26:52 +0900

    0503

---
+++ string_sample.py
@@ -0,0 +1,29 @@
+print("""
+aaa
+bbb
+ccc
+""")
+print("aaa¥naaa")
+print('aaa'*5)
+print('aaa'+'bbb')
+print('abcde'[4])
+print('abcde'[1:3])
+print('abcde'[1:])
+print('abcde'[-2])
+print('abcddddddddde'.count('d'))
+print(len('abcde'))
+
+print(5 <= 6 and 6 >= 7)
+print(5 <= 6 or 6 >= 7)
+print(1 < 2 < 3)
+print(1 < 2 > 3)
+
+a = 'a'
+a = 1
+a = a + 1
+a += 10
+a -= 5
+print(a)
+
+c, d = 1, 2
+print(c, d)
