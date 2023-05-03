ba9049b5275faa35428147574e92dd9243111cb7
sakon-ziwave <ryo.development0320@gmail.com>
2023-05-03 11:43:19 +0900

    0503

--- dict_sample.py
+++
@@ -1,8 +0,0 @@
-# 連想配列
-menu = {'coffee': 300, 'cafe latte': 400, 'tea': 300}
-print(menu['coffee'])
-menu['milk'] = 200
-print(menu)
-
-print(menu.keys())
-print(menu.values())
--- modules/calculator.py
+++
@@ -1,2 +0,0 @@
-def calc_add(a, b):
-  return a + b
